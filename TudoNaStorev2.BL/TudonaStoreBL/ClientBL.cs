5c590c7afaa1cb40f19f79a7971638e836226c44
carlos_oliveira@swissre.com <carlos_oliveira@swissre.com>
2021-07-01 20:26:13 -0300

    ajuste de pastas

---
+++ TudoNaStorev2.BL/TudonaStoreBL/ClientBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TudoNaStoreEntity;
+using TudoNaStoreDAO;
+
+namespace TudonaStoreBL
+{
+    public class ClientBL
+    {
+        public static void InsertClient(ClientEntity obj)
+        {
+            ClientDAO.InsertClient(obj);
+        }
+    }
+}
---
+++ TudoNaStorev2.BL/TudonaStoreBL/EntityGenericBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+using TudonaStoreBL;
+using TudoNaStoreEntity;
+using TudoNaStoreDAO;
+
+namespace TudonaStoreBL
+{
+    public static class EntityGenericBL
+    {
+        public static List<EntityGenericModal> GetRegionValues()
+        {
+            return GenericDAO.GetRegionValues();
+        }
+
+        public static List<EntityGenericModal> GetStatusValues(string EntityIdentifier)
+        {
+            return GenericDAO.GetEntityStatusValues(EntityIdentifier);
+        }
+    }
+}
